9d535b366bff644ddf6fcd147610eed18920011e
Angelo Mesquita <angelo.mesquita@professor.unis.edu.br>
2022-10-26 21:08:31 -0300

    refatora o ChirpController

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,105 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Chirp\{StoreChirpRequest, UpdateChirpRequest};
 use App\Models\Chirp;
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
+use Inertia\Response as InertiaResponse;
 
 class ChirpController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(): InertiaResponse
     {
-        return Inertia::render('Chirps/Index', [
-            'chirps' => Chirp::with('user:id,name')->latest()->get(),
-        ]);
+        $chirps = Chirp::with('user:id,name')->latest()->get();
+        $props = ['chirps' => $chirps];
+        return Inertia::render('Chirps/Index', $props);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $validated = $request->validate([
-            'message' => 'required|string|max:255',
-        ]);
-        
-        $request->user()->chirps()->create($validated);
- 
+    public function store(StoreChirpRequest $request): RedirectResponse
+    {   
+        $request->user()->chirps()->create($request->all());
         return redirect(route('chirps.index'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Chirp  $chirp
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Chirp $chirp)
+    public function update(UpdateChirpRequest $request, Chirp $chirp): RedirectResponse
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Chirp  $chirp
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Chirp $chirp)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Chirp  $chirp
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Chirp $chirp)
-    {
-        $this->authorize('update', $chirp);
- 
-        $validated = $request->validate([
-            'message' => 'required|string|max:255',
-        ]);
- 
-        $chirp->update($validated);
- 
+        $chirp->update($request->all());
         return redirect(route('chirps.index'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Chirp  $chirp
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Chirp $chirp)
+    public function destroy(Chirp $chirp): RedirectResponse
     {
         $this->authorize('delete', $chirp);
- 
         $chirp->delete();
- 
         return redirect(route('chirps.index'));
     }
 }
